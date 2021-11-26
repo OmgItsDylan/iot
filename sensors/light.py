d37a5928c509eae272c57cc0d0ec2eb518dbab81
dylan <dylan.ferrari@epitech.eu>
2021-11-27 00:59:43 +0100

    added communication between app and server

--- sensors/light.py
+++ sensors/light.py
@@ -3,28 +3,33 @@
 import sys
 import random
 import time
+import os
 
-def lightPower():
-  light_status = random.choice(['on', 'off'])
-  return light_status
+# 9KoWvn7JUZ1xeoVUJPtr
+# 8lDJcqe7waDWdpnp56Ye
+# VhB5McEIlaOY4YQorCnB
 
-def lightIntensity():
-  light_level = random.randint(0, 100)
-  return light_level
+def lightIntensity(status):
+  if status == "on":
+    light_level = random.randint(0, 100)
+  else:
+    light_level = 0
+  return '{"intensity":' + str(light_level) + '}'
 
 def lightStatus():
-  light_status = random.choice(['on', 'off'])
-  return light_status
+  light_status = random.choice(['on', 'off', 'on', 'on', 'on'])
+  return '{"status":' + light_status + '}', light_status
 
 def main():
   if sys.argv and len(sys.argv) > 1:
     access_code = sys.argv[1]
+    cmd = 'mosquitto_pub -d -q 1 -h "192.168.1.42" -p "1883" -t "v1/devices/me/telemetry" -u "' + access_code + '" -m '
     while True:
-      light_power = lightPower()
-      light_level = lightIntensity()
-      light_status = lightStatus()
-      print(light_power, light_level, light_status)
-      time.sleep(2)
+      msgStatus, status = lightStatus()
+      msgIntensity = lightIntensity(status)
+      os.system(cmd + msgStatus)
+      os.system(cmd + msgIntensity)
+      time.sleep(3)
   else:
     print("Usage: python3 light.py access_code")
 
