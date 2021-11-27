30275a983972623044477659ba9c9ea5e2a23aab
dylan <dylan.ferrari@epitech.eu>
2021-11-27 01:21:25 +0100

    added communication between server and app

--- sensors/temperature_humidity.py
+++ sensors/temperature_humidity.py
@@ -3,29 +3,33 @@
 import sys
 import random
 import time
+import os
+
+# EwgZPbrg0ZCMTQ7i9FRr
+# t0ribl0rddawhoFYYccw
+# PNLCvQREWEG5gIYZkXDD
 
 def temperature():
   celsius = random.randint(-10, 43)
-  return celsius
+  return '"{\"temperature\": ' + str(celsius) + '}"'
 
 def humidity():
   percentage = random.randint(0, 100)
-  return percentage
-
-def position():
-  lat = random.randint(-90, 90)
-  long = random.randint(-180, 80)
-  return [lat + random.random(), long + random.random()]
+  return '"{\"humidity\": ' + str(percentage) + '}"'
 
 def main():
   if sys.argv and len(sys.argv) > 1:
     access_code = sys.argv[1]
+
+    cmd = 'curl -v -X POST -d '
+    cmd2 = ' http://192.168.1.42:9090/api/v1/' + access_code + '/telemetry --header "Content-Type:application/json"'
+
     while True:
-      celsius = temperature()
-      percentage = humidity()
-      coordinates = position()
-      print(celsius, percentage, coordinates)
-      time.sleep(2)
+      msgTemp = temperature()
+      msgHumi = humidity()
+      os.system(cmd + msgTemp + cmd2)
+      os.system(cmd + msgHumi + cmd2)
+      time.sleep(3)
   else:
     print("Usage: python3 temperature_humidity.py access_code")
 
