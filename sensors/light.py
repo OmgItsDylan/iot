2399eaa39f3a0d66e7bfdd9d39bbe29e78960446
dylan <dylan.ferrari@epitech.eu>
2021-11-28 22:04:40 +0100

    added basic GUI for light sensor

--- sensors/light.py
+++ sensors/light.py
@@ -4,6 +4,7 @@ import sys
 import random
 import time
 import os
+import tkinter as tk
 
 # 9KoWvn7JUZ1xeoVUJPtr
 # 8lDJcqe7waDWdpnp56Ye
@@ -14,24 +15,46 @@ def lightIntensity(status):
     light_level = random.randint(0, 100)
   else:
     light_level = 0
+  global var_int
+  var_int.set("Light intensity: " + str(light_level))
   return '{"intensity":' + str(light_level) + '}'
 
 def lightStatus():
   light_status = random.choice(['on', 'off', 'on', 'on', 'on'])
+  global var_status
+  var_status.set("Status: " + light_status)
   return '{"status":' + light_status + '}', light_status
 
+
+def loop():
+    msgStatus, status = lightStatus()
+    msgIntensity = lightIntensity(status)
+    os.system(cmd + msgStatus)
+    os.system(cmd + msgIntensity)
+    win.update_idletasks()
+    win.after(3000, loop)
+
+
 def main():
-  if sys.argv and len(sys.argv) > 1:
-    access_code = sys.argv[1]
-    cmd = 'mosquitto_pub -d -q 1 -h "192.168.1.42" -p "1883" -t "v1/devices/me/telemetry" -u "' + access_code + '" -m '
-    while True:
-      msgStatus, status = lightStatus()
-      msgIntensity = lightIntensity(status)
-      os.system(cmd + msgStatus)
-      os.system(cmd + msgIntensity)
-      time.sleep(3)
-  else:
-    print("Usage: python3 light.py access_code")
+    global win
+    global var_status
+    global var_int
+    win = tk.Tk()
+    win.geometry("700x350")
+    var_status = tk.StringVar()
+    var_status.set('Unknown')
+    var_int = tk.StringVar()
+    var_int.set('Unknown')
+    label1 = tk.Label(win, textvariable=var_status).pack()
+    label2 = tk.Label(win, textvariable=var_int).pack()
+    win.after(1000, loop)
+    win.mainloop()
+
 
 if __name__ == '__main__':
-    main()
+    if sys.argv and len(sys.argv) > 1:
+        access_code = sys.argv[1]
+        cmd = 'mosquitto_pub -d -q 1 -h "192.168.1.42" -p "1883" -t "v1/devices/me/telemetry" -u "' + access_code + '" -m '
+        main()
+    else:
+        print("Usage: python3 light.py access_code")
